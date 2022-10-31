87ca5cc4c6bda72a6b33f3091363c433d6c958ab
Dpenedo <dpenedo@tutanota.com>
2022-10-31 14:15:34 -0300

    clase 3 de la unidad 2 terminada

--- homepage/urls.py
+++ homepage/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from homepage import views
+from homepage.views import Templatetags1
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('templatetags1', Templatetags1.as_view(), name="templatetags1"),
 
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q
 from productos.models import Producto
+from django.views.generic import View
+import datetime
 import homepage
 
 
@@ -13,4 +15,26 @@ def index(request):
 
 
     return render(request, 'homepage/index.html', params)
-# Create your views here.
+
+
+
+class Templatetags1(View):
+    template = "homepage/templatetags1.html"
+    def get(self, request):
+        params = {}
+        params["cross_site_scripting"]="""
+            <script>
+            $("*").css({
+                "background-color": "yellow",
+                "font-weight": "bolder",
+                "margin-bottom": "200px",
+            });
+            </script>
+        
+        """
+        params["fecha_de_hoy"]=datetime.datetime.now()
+        params["mi_lista"]=[1,2,3,4,5,6,7,8]
+        params["row3"]="row3"
+        params["mi_lista2"]=[]
+
+        return render(request, self.template, params)
